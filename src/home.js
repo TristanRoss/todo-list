a70d346c646fb88f3cfad036581e8ecbcbbd576f
TristanRoss <tgr1998@live.com>
2020-07-23 08:53:58 -0500

    Update todoPage.js

--- src/home.js
+++ src/home.js
@@ -6,7 +6,8 @@ function createHome() {
     const div = document.createElement('div');
     div.className = "project";
     let project = new Project([], "Project 1");
-    project.add(new Todo('blah title', 'blah', 'blah', 'blah', 'blah'));
+    project.add(new Todo('Grocery Shopping', 'Get some groceries', 'August 13 2020', '1', 'These some notes'));
+    project.add(new Todo('Grocery Shopping 2', 'Get some more groceries', 'August 14 2020', '2', 'These some more notes'));
     div.textContent = project.title;
     div.addEventListener('click', () => {
         createTodoPage(project);
--- src/todoPage.js
+++ src/todoPage.js
@@ -4,12 +4,30 @@ function createTodoPage(project) {
     deleteElements();
     const body = document.querySelector('body');
     for (let i = 0; i < project.todos.length; i++) {
-        const div = document.createElement('div');
-        div.className = "todo";
-        let todo = project.todos[i];
-        div.textContent = todo.title;
+        const div = makeTodoDiv(project.todos[i]);
         body.appendChild(div);
     }
+}
+
+function makeTodoDiv(todo) {
+    const div = document.createElement('div');
+    div.className = "todo";
+    const title = document.createElement('h2');
+    const description = document.createElement('p');
+    const dueDate = document.createElement('p');
+    const priority = document.createElement('p');
+    const notes = document.createElement('p');
+    title.textContent = todo.title;
+    description.textContent = todo.description;
+    dueDate.textContent = todo.description;
+    priority.textContent = todo.priority;
+    notes.textContent = todo.notes;
+    div.appendChild(title);
+    div.appendChild(description);
+    div.appendChild(dueDate);
+    div.appendChild(priority);
+    div.appendChild(notes);
+    return div;
 }
 
 export {
